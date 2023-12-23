b7178751bf338e3555cb46bc48b6622421c29f7c
taras55001 <taras5500@gmail.com>
2023-12-23 20:12:41 +0100

    work

--- chat/pdf/urls.py
+++ chat/pdf/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+
+from django.urls import include,path
 
 from . import views
 
@@ -8,5 +9,4 @@ urlpatterns = [
     path('upload/', views.upload_file, name='upload_file'),
     path('download/<int:file_id>/', views.download_file, name='download_file'),
     path('delete_file/<int:file_id>/', views.delete_file, name='delete_file'),
-
 ]
