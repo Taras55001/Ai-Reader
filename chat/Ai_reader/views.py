a24682697a325e97e2b41d7fc25c240094860045
taras55001 <taras5500@gmail.com>
2024-01-03 23:56:03 +0100

    ok

--- chat/Ai_reader/views.py
+++ chat/Ai_reader/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.urls import reverse
 from .models import Chat, Message
+from .dolly_answer import answer as ans
+
 
 def main(request):
-    return render(request, 'Ai_reader/main.html')
+    return render(request, "Ai_reader/main.html")
+
 
 def chat(request):
     user = request.user
@@ -11,30 +14,40 @@ def chat(request):
         user_chats = Chat.objects.filter(users=user)
 
         if not user_chats.exists():
-
             new_chat = Chat.objects.create()
             new_chat.users.add(user)
             new_chat.save()
 
             chat_replies = []
-            return render(request, 'Ai_reader/chat.html', {
-                'user_chats': user_chats,
-                'current_chat': new_chat,
-                'chat_replies': chat_replies,
-            })
+            return render(
+                request,
+                "Ai_reader/chat.html",
+                {
+                    "user_chats": user_chats,
+                    "current_chat": new_chat,
+                    "chat_replies": chat_replies,
+                },
+            )
         else:
             current_chat = user_chats.first()
             chat_replies = Message.objects.filter(chat=current_chat)
 
-            return render(request, 'Ai_reader/chat.html', {
-                'user_chats': user_chats,
-                'current_chat': current_chat,
-                'chat_replies': chat_replies,
-            })
+            return render(
+                request,
+                "Ai_reader/chat.html",
+                {
+                    "user_chats": user_chats,
+                    "current_chat": current_chat,
+                    "chat_replies": chat_replies,
+                },
+            )
     else:
-        return redirect(reverse('users:eror_aut')) 
+        return redirect(reverse("users:eror_aut"))
 
 
 def answer(request):
-    
-    return render(request, 'Ai_reader/chat.html')
+    ans(
+        filename=r"chat\media\uploads\PlayerGuide.pdf",
+        question="How many players in the game?",
+    )
+    return render(request, "Ai_reader/chat.html")
--- chat/pdf/models.py
+++ chat/pdf/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class UploadedFile(models.Model):
-    file = models.FileField(upload_to='uploads/')
+    file = models.FileField()
+    vector_db = models.FileField()
     uploaded_at = models.DateTimeField(auto_now_add=True)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, default=None)
+    user_id = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, blank=True, default=None
+    )
