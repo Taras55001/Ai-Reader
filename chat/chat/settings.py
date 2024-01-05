aec3ec217dc04efd581272466c32f45da0f86088
taras55001 <taras5500@gmail.com>
2024-01-05 11:05:05 +0100

    1

--- chat/chat/settings.py
+++ chat/chat/settings.py
@@ -29,7 +29,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
