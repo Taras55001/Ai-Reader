9e609dc0bb3d270ad2c13051ba546faa4a88229a
taras55001 <taras5500@gmail.com>
2024-01-05 03:07:02 +0100

    docker build

--- chat/chat/settings.py
+++ chat/chat/settings.py
@@ -28,11 +28,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["*", "http://127.0.0.1:8000", "https://ai-reader.onrender.com"]
 
 # Application definition
 
