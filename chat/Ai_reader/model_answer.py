4fc7f024c1c8a9b08d6687457088110ebe6f3b95
Oleksandr Tarasov <123240727+Taras55001@users.noreply.github.com>
2024-01-07 01:43:33 +0100

    Update model_answer.py

--- chat/Ai_reader/model_answer.py
+++ chat/Ai_reader/model_answer.py
@@ -23,5 +23,4 @@ def answer(filename: UploadedFile, question: str) -> str:
     vector_store = pickle.load(filename.vector_db)
 
     context = vector_store.similarity_search(query=question, k=3)
-    print(context)
     return gen_text(context, question)
